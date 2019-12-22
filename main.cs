ba2131219618b8c6999e8c98d49441e122c750f1
Redmond Chan <redmondchan92@gmail.com>
2019-12-22 20:01:42 +0000

    fixed spacing of comments and examples

--- main.cs
+++ main.cs
@@ -4,10 +4,10 @@ class MainClass {
   public static void Main (string[] args) {
 
     // nested example
+
     Console.WriteLine ("How old are you?");
     int yourAge;
     var input = Int32.TryParse(Console.ReadLine(), out yourAge);
-
     if (input){
       if(yourAge < 27){
         Console.WriteLine("You are younger than me.");
@@ -21,7 +21,9 @@ class MainClass {
       Console.WriteLine("Invalid Input. Please enter a valid number.");
     };
 
+
     // else if example
+
     // Console.WriteLine ("How old are you?");
     // var yourAge = Convert.ToInt32(Console.ReadLine());
     // if(yourAge < 27){
@@ -32,7 +34,9 @@ class MainClass {
     //   Console.WriteLine("You are NOT younger than me.");
     // };
 
+
     // else example
+
     // Console.WriteLine ("How old are you?");
     // var yourAge = Convert.ToInt32(Console.ReadLine());
     // if(yourAge < 27){
@@ -41,7 +45,9 @@ class MainClass {
     //   Console.WriteLine("You are NOT younger than me.");
     // };
 
+
     // if statement example
+
     // Console.WriteLine ("How old are you?");
     // var yourAge = Convert.ToInt32(Console.ReadLine());
     // if(yourAge < 27){
