eddcca595626102c0f96c2e87a1fc3112524d31f
Redmond Chan <redmondchan92@gmail.com>
2019-12-22 18:55:50 +0000

    Wrote if else example

--- main.cs
+++ main.cs
@@ -2,7 +2,14 @@ using System;
 
 class MainClass {
   public static void Main (string[] args) {
-    Console.WriteLine ("Hello WorldTest");
+
+    Console.WriteLine ("How old are you?");
+    var yourAge = Convert.ToInt32(Console.ReadLine());
+    if(yourAge < 27){
+      Console.WriteLine("You are younger than me.");
+    } else {
+      Console.WriteLine("I'm older than you.");
+    };
 
   }
 }
