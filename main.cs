bf30a8f76cf2fbd69c14d59a83c35db019ba86e0
Redmond Chan <redmondchan92@gmail.com>
2019-12-22 19:20:47 +0000

    added comments and previous examples

--- main.cs
+++ main.cs
@@ -14,5 +14,35 @@ class MainClass {
       Console.WriteLine("You are NOT younger than me.");
     };
 
+    // else if example
+    // Console.WriteLine ("How old are you?");
+    // var yourAge = Convert.ToInt32(Console.ReadLine());
+    // Console.WriteLine($"{yourAge}");
+    // if(yourAge < 27){
+    //   Console.WriteLine("You are younger than me.");
+    // } else if (yourAge > 27) {
+    //   Console.WriteLine("You are older than me.");
+    // } else {
+    //   Console.WriteLine("You are NOT younger than me.");
+    // };
+
+    // else example
+    // Console.WriteLine ("How old are you?");
+    // var yourAge = Convert.ToInt32(Console.ReadLine());
+    // Console.WriteLine($"{yourAge}");
+    // if(yourAge < 27){
+    //   Console.WriteLine("You are younger than me.");
+    // } else {
+    //   Console.WriteLine("You are NOT younger than me.");
+    // };
+
+    // if statement example
+    // Console.WriteLine ("How old are you?");
+    // var yourAge = Convert.ToInt32(Console.ReadLine());
+    // Console.WriteLine($"{yourAge}");
+    // if(yourAge < 27){
+    //   Console.WriteLine("You are younger than me.");
+    // };
+
   }
 }
